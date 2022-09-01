cb52dbfde3570f3672c3b65086bd497fd524ad1d
Wesley Miranda De Oliveira <wesley.oliveira@cronapp.io>
2022-09-01 13:51:11 +0000

    2.0

--- src/main/java/blockly/Bloco.java
+++ src/main/java/blockly/Bloco.java
@@ -15,15 +15,25 @@ public static final int TIMEOUT = 300;
 /**
  *
  * @author Wesley Miranda De Oliveira
- * @since 01/09/2022 09:59:06
+ * @since 01/09/2022 10:49:36
  *
  */
 public static Var fa_C3_A7a_algo() throws Exception {
  return new Callable<Var>() {
 
+   private Var Usuario = Var.VAR_NULL;
+   private Var UsuarioId = Var.VAR_NULL;
    private Var item = Var.VAR_NULL;
 
    public Var call() throws Exception {
+    Usuario =
+    cronapi.list.Operations.getFirst((
+    cronapi.database.Operations.query(Var.valueOf("app.entity.User"),Var.valueOf("select u.name from User u where u.userName = :userName"),Var.valueOf("userName",
+    cronapi.util.Operations.getCurrentUserName()))));
+    UsuarioId =
+    cronapi.list.Operations.getFirst((
+    cronapi.database.Operations.query(Var.valueOf("app.entity.User"),Var.valueOf("select u.id from User u where u.userName = :userName"),Var.valueOf("userName",
+    cronapi.util.Operations.getCurrentUserName()))));
     item =
     cronapi.dateTime.Operations.formatDateTime2(
     cronapi.dateTime.Operations.getNow(),
@@ -39,15 +49,28 @@ public static Var fa_C3_A7a_algo() throws Exception {
     cronapi.util.Operations.callClientFunction(Var.valueOf("cronapi.screen.changeValueOfField"),
     Var.valueOf("COMENTARIO_POCGrid.active.dataUltimaAtualizacaoComentario"), item);
     cronapi.util.Operations.callClientFunction(Var.valueOf("cronapi.screen.changeValueOfField"),
-    Var.valueOf("COMENTARIO_POCGrid.active.autorComentario_name"),
-    cronapi.list.Operations.getFirst((
-    cronapi.database.Operations.query(Var.valueOf("app.entity.User"),Var.valueOf("select u.name from User u where u.userName = :userName"),Var.valueOf("userName",
-    cronapi.util.Operations.getCurrentUserName())))));
+    Var.valueOf("COMENTARIO_POCGrid.active.autorComentario"), UsuarioId);
     cronapi.util.Operations.callClientFunction(Var.valueOf("cronapi.screen.changeValueOfField"),
-    Var.valueOf("COMENTARIO_POCGrid.active.autorComentario"),
-    cronapi.list.Operations.getFirst((
-    cronapi.database.Operations.query(Var.valueOf("app.entity.User"),Var.valueOf("select u.name from User u where u.userName = :userName"),Var.valueOf("userName",
-    cronapi.util.Operations.getCurrentUserName())))));
+    Var.valueOf("COMENTARIO_POCGrid.active.autorComentario_name"), Usuario);
+    return Var.VAR_NULL;
+   }
+ }.call();
+}
+
+/**
+ *
+ * @author Wesley Miranda De Oliveira
+ * @since 01/09/2022 10:49:36
+ *
+ */
+public static Var fa_C3_A7a_algo2() throws Exception {
+ return new Callable<Var>() {
+
+   private Var Usuario = Var.VAR_NULL;
+   private Var UsuarioId = Var.VAR_NULL;
+   private Var item = Var.VAR_NULL;
+
+   public Var call() throws Exception {
     return Var.VAR_NULL;
    }
  }.call();
@@ -58,7 +81,7 @@ public static Var fa_C3_A7a_algo() throws Exception {
  * Bloco
  *
  * @author Wesley Miranda De Oliveira
- * @since 01/09/2022 09:59:06
+ * @since 01/09/2022 10:49:36
  *
  */
 public static Var userOn() throws Exception {
